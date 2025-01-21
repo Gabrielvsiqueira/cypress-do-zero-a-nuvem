027121cfd91671dbca1de6fe91d6be16c40575ae
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-01-21 16:20:57 -0300

    Exercicios da aula 2

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,9 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', () => {
+    cy.get('input[name="firstName"]').type('Gabriel Vitor', {delay: 100})
+    cy.get('input[name="lastName"]').type('Siqueira', {delay: 100})
+    cy.get('input[name="phone"]').eq(0).should('have.value', '').type(999570539, {delay: 100})
+    cy.get('input[name="email"]').eq(0).type('gabesiqueira25@gmail.com', {delay: 100})
+    cy.get('textarea[name="open-text-area"]').type('Teste.', {delay: 100})
+    cy.get('button[type="submit"]').click()
+}) 
+
